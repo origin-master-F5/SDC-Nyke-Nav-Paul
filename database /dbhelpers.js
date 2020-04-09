47ee6570d10f8d54c9505a3c9563425c38405234
paul-11 <mrpaulyi@gmail.com>
2020-04-08 18:51:51 -0700

    Delete unnecessary files and folders.
    Deleted the old database folders, along with the files inside it.

--- database /dbhelpers.js
+++
@@ -1,7 +0,0 @@
-const db = require('./index.js');
-
-module.exports = {
-    find: ({ keyword }) => {
-       return db.find().or([{ collections: { $regex: new RegExp(keyword, 'i') } }, { type: { $regex: new RegExp(keyword, 'i') }}])
-    }
-}
--- database-final/dbhelpers.js
+++
@@ -1,9 +0,0 @@
-// const db = require('./index.js');
-
-// module.exports = {
-//     find: ({ keyword }) => {
-//         console.log('this be the word ', keyword)
-//         return db.find().or([{ collections: { $regex: new RegExp(keyword, 'i') } }, { type: { $regex: new RegExp(keyword, 'i') }}])
-//         // return db.find({collections: keyword, type: keyword})
-//     }
-// }
